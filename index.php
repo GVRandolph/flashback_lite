28d3e951a1916b45bcaadc5c01ff3bebff0b373a
llebielle <lucas@lebielle.com>
2022-07-03 16:19:39 -0400

    tuto

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+    $debug = true;
+    $v = '1.0';
+
+?><!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/flashback.css?3">
+  <link rel="stylesheet" href="css/splash.css?2">
+
+  <meta name="theme-color" content="#fafafa">
+
+
+</head>
+
+<body>
+<!-- ----------------------------------------------------------splash screen ------------------------------ -->
+<div class="splash">
+    <img class="logo" src="img/game/FB_logo.png" alt="logo FlashBack Zombie kidz"/>
+    <p>
+      Bienvenue dans le tutoriel jouable de FlashBack Zombie kidz.
+    </p>
+    <p>
+
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel magna at orci convallis placerat. Cras hendrerit ullamcorper turpis at hendrerit. Morbi vitae nisi id tellus euismod feugiat. Donec diam nibh, pulvinar a ex vitae, euismod pharetra urna. Curabitur iaculis egestas consectetur. Vestibulum bibendum massa urna, ut mollis turpis varius at. Phasellus nec odio ut urna ultrices efficitur at a velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam in vehicula nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel magna at orci convallis placerat. Cras hendrerit ullamcorper turpis at hendrerit. Morbi vitae nisi id tellus euismod feugiat. Donec diam nibh, pulvinar a ex vitae, euismod pharetra urna. Curabitur iaculis egestas consectetur. Vestibulum bibendum massa urna, ut mollis turpis varius at. Phasellus nec odio ut urna ultrices efficitur at a velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam in vehicula nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel magna at orci convallis placerat. Cras hendrerit ullamcorper turpis at hendrerit. Morbi vitae nisi id tellus euismod feugiat. Donec diam nibh, pulvinar a ex vitae, euismod pharetra urna. Curabitur iaculis egestas consectetur. Vestibulum bibendum massa urna, ut mollis turpis varius at. Phasellus nec odio ut urna ultrices efficitur at a velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam in vehicula nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae
+      Lorem ipsum dolor it amet, consectetur adipiscing elit. Cras vel magna at orci convallis placerat. Cras hendrerit ullamcorper turpis at hendrerit. Morbi vitae nisi id tellus euismod feugiat. Donec diam nibh, pulvinar a ex vitae, euismod pharetra urna. Curabitur iaculis egestas consectetur. Vestibulum bibendum massa urna, ut mollis turpis varius at. Phasellus nec odio ut urna ultrices efficitur at a velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam in vehicula nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel magna at orci convallis placerat. Cras hendrerit ullamcorper turpis at hendrerit. Morbi vitae nisi id tellus euismod feugiat. Donec diam nibh, pulvinar a ex vitae, euismod pharetra urna. Curabitur iaculis egestas consectetur. Vestibulum bibendum massa urna, ut mollis turpis varius at. Phasellus nec odio ut urna ultrices efficitur at a velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam in vehicula nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae
+
+
+    </p>
+    <button>
+    <a href="game.html">
+
+            Commencer
+
+    </a>
+    </button>
+
+</div>
+</body>
+
+</html>
